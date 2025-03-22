142e092f5ac9ebc4c4d9033a3a8a74b655254296
Mahisidda <your.email@example.com>
2025-03-21 17:55:43 -0700

    Make layout responsive for mobile devices

--- src/app/layout.js
+++ src/app/layout.js
@@ -21,10 +21,10 @@ export default function RootLayout({ children }) {
   return (
     <html lang="en">
       <body
-        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#F2F2F2]`}
+        className={`${geistSans.variable} ${geistMono.variable} antialiased bg-[#F2F2F2] flex min-h-screen`}
       >
         <Sidebar />
-        <main className="flex-1 ml-64 p-8">
+        <main className="flex-1 md:ml-64 p-4 md:p-8 w-full">
           {children}
         </main>
       </body>
